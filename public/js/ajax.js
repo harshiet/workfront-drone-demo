04de1c5002c5f2139a046be8f760c44c048e3753
harshiet <harsh.agarwal82@gmail.com>
2015-05-11 23:29:31 -0400

    1

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,57 +1,61 @@
-var doProcess = function() {
-
-	var taskCompleteDelay = 100;
-	var sessionID = '';
+var taskCompleteDelay = 100;
+var sessionID = '';
+var droneTasks;
+var login = function() {
 	$.ajax({
 		url : '/login',
 		success : function(data) {
+			console.log(data);
 			sessionID = data;
-			getDroneTasks();
+		},
+		async : false
+	});
+};
+
+var getDroneTasks = function() {
+	$.ajax({
+		url : '/tasks?sessionID=' + sessionID,
+		async : false,
+		success : function(tasks) {
+			droneTasks = tasks;
+			$.each(tasks, function(i, task) {
+				$('#tasks').append('<tr><td>' + task.taskNumber + '</td><td>' + task.name + '</td><td id="td_' + task.taskNumber + '"></td></tr>');
+			})
 		}
 	});
+};
 
-	var getDroneTasks = function() {
-		$.ajax({
-			url : '/tasks?sessionID=' + sessionID,
-			success : function(tasks) {
-				setTimeout(function() {
-					completeTask(0, tasks);
-				}, taskCompleteDelay);
-			}
-		});
-	};
-
-	var completeTask = function(i, tasks) {
-		$.ajax({
-			url : '/complete?taskID=' + tasks[i].ID + '&sessionID=' + sessionID,
-			success : function(data) {
-				console.log(data);
-				setTimeout(function() {
-					if (i < tasks.length - 84) {
-						setTimeout(function() {
-							completeTask(i + 1, tasks);
-						}, taskCompleteDelay);
-					} else {
-						poll(tasks[i + 1]);
-					}
-				}, taskCompleteDelay);
-			}
-		});
-	};
-
-	var poll = function(task) {
-		$.ajax({
-			url : '/poll?taskID=' + task.ID + '&sessionID=' + sessionID,
-			success : function(data) {
-				if (data == 'CPL') {
-					console.log('launch drone');
-				} else {
+var completeTask = function(i) {
+	$.ajax({
+		url : '/complete?taskID=' + droneTasks[i].ID + '&sessionID=' + sessionID,
+		success : function(data) {
+			$('#td_' + droneTasks[i].taskNumber).html('Complete');
+			$('#progress-bar').css('width', ((i * 100) / droneTasks.length) + '%');
+			$('#progress-bar').html(Math.round(((i * 100) / droneTasks.length), 0) + '%');
+			setTimeout(function() {
+				if (i < droneTasks.length - 50) {
 					setTimeout(function() {
-						poll(task);
+						completeTask(i + 1);
 					}, taskCompleteDelay);
+				} else {
+					poll(droneTasks[i + 1]);
 				}
-			}
-		});
-	}
+			}, taskCompleteDelay);
+		}
+	});
+};
 
+var poll = function(task) {
+	$.ajax({
+		url : '/poll?taskID=' + task.ID + '&sessionID=' + sessionID,
+		success : function(data) {
+			if (data == 'CPL') {
+				console.log('launch drone');
+			} else {
+				setTimeout(function() {
+					poll(task);
+				}, taskCompleteDelay);
+			}
+		}
+	});
 }
